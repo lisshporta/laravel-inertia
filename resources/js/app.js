14f765465555d794d8a31981e03671823efc01a3
lisshporta <lis.shporta@gmail.com>
2023-02-21 16:38:45 +0100

    Progress Indicators

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 
 createInertiaApp({
+  progress: {},
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     return pages[`./Pages/${name}.vue`]
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/users', function () {
+    sleep(2);
     return inertia('Users');
 });
 
