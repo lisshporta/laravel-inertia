0ceab49d5154870f991d4edf63a7fe57bc5015cc
lisshporta <lis.shporta@gmail.com>
2023-02-22 12:53:02 +0100

    Perform Non-GET Requests

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('/users', function () {
 Route::get('/settings', function () {
     return inertia('Settings');
 });
+
+Route::post('/logout', function() {
+    dd('logging the user out');
+});
