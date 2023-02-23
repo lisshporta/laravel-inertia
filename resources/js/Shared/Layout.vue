278db0475e83e83b520e4bd482310fc685357794
lisshporta <lis.shporta@gmail.com>
2023-02-23 10:44:44 +0100

    Shared Data

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,8 +1,13 @@
 <template>
     <section class="p-6 bg-gray-200">
       <header class="flex justify-between">
+        <div class="flex items-center">
         <h1 class="font-bold text-lg">My App</h1>
 
+        <p class="test-sm ml-4">
+          Welcome Back, {{ username }}!
+        </p>
+        </div>
         <Nav />
       </header>
     </section>
@@ -20,6 +25,12 @@ import Nav from './Nav.vue';
 
 export default {
 
-    components: { Nav }
+    components: { Nav },
+
+    computed: {
+      username() {
+        return this.$page.props.auth.user.username;
+      }
+    }
 }
 </script>
